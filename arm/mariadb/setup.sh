7db7ca82a533f7c6a94fff26fc34459a7fdb8891
Ivan Chavero <ichavero@redhat.com>
2016-07-06 01:30:13 -0600

    Add password to mariadb on setup

--- arm/mariadb/setup.sh
+++ arm/mariadb/setup.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-echo "setupiando ando"
+# Set up MariaDB
+
+if [[ $1 != "" ]]; then
+    PASSWORD=$1
+else 
+    PASSWORD="sotolito"
+fi
+
+echo "Setting up MariaDB"
+
 
 
 mkdir /var/sotolito/
 cd /var/sotolito 
 tar xvf /volconf.tar
 
+mysqladmin -u root password $PASSWORD
