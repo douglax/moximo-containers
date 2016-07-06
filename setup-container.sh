1fb30972f0ce3b9092af453c88d436fd36c9dab9
Ivan Chavero <ichavero@redhat.com>
2016-07-06 01:43:54 -0600

    Add general setup script

---
+++ setup-container.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Creates a moximo container
+
+CONTAINER_PATH="./x86_64"
+CONTAINER=""
+PASSWORD=""
+
+while getopts "p:c:P:" opt; do
+  case $opt in
+    c)
+      CONTAINER=$OPTARG
+      ;;
+    p)
+      CONTAINER_PATH=$OPTARG
+      ;;
+    P)
+      PASSWORD=$OPTARG
+      ;;
+
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
+
+cd $CONTAINER_PATH/$CONTAINER
+
+#sudo dnf install -y `cat rpms.txt`
+
+dnf info `cat rpms.txt`
+
+ls dockerize-$CONTAINER 
+ls dockerize-$CONTAINER-setup $PASSWORD
