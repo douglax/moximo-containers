9e9cca4bf8ef131e2094f70d03b57dae87133df3
Ivan Chavero <ichavero@redhat.com>
2016-07-07 13:50:55 -0600

    Add general setup script

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Install dockerize
+
+DOCKERIZE=`which dockerize`
+
+function setup_pkg_manager {
+    YUM=`which dnf`
+    if [[ ${YUM} == '' ]]; then
+        YUM=`wich yum`
+    fi
+}
+
+function setup_dockerize {
+    if [[ ${DOCKERIZE} == '' ]]; then
+        $YUM install -y python-pyelftools.noarch
+        git clone https://github.com/larsks/dockerize dockerize.git
+        cd dockerize.git
+        python setup.py install    
+
+    fi
+}
